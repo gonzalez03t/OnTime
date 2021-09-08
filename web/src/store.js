bcad680aab938cfbb3d3983969886e2f7e8ea25a
Rodrigo Lobo <32112221+rodrigolobo25@users.noreply.github.com>
2021-09-07 22:45:21 -0400

    React store (#32)
    * added basic state store
    
    * added dependency
    
    * wrapped user on object
    
    * changed the setUser method a bit, still getting used to it, but should be ready now
    
    * chaned the fullname method
    
    * added public folder back
    
    * Update store.js
    
    Co-authored-by: Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>

---
+++ web/src/store.js
@@ -0,0 +1,20 @@
+import create from "zustand"
+
+const useStore = create((set, get) => ({
+    user: null,
+
+    fullname: () => `${get().user["firstName"]} ${get().user["lastName"]}`,
+
+    setUser: (newUser) => { 
+      if(!newUser){
+        console.error("State not updated. User object is empty.")
+      }else{
+        set((state) => ({
+          ...state,
+          user: newUser
+        }))
+      }
+    }
+}))
+
+export default useStore;
