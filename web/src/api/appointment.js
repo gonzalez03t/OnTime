c6de61cf0b878d0b6d4911ac423f7effdcb02668
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2021-09-09 12:00:49 -0700

    #16: added frontend functions to call backend for appointment crud

---
+++ web/src/api/appointment.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const baseUrl = process.env.REACT_APP_BACKEND_URL + '/api/appointments';
+
+export function getUserAppointments() {
+  return axios.get(baseUrl).catch((err) => err.response);
+}
+
+export function createUserAppointment(employeeEmail, startsAt, wantsReminder) {
+  return axios
+    .post(baseUrl + '/new', { employeeEmail, startsAt, wantsReminder })
+    .catch((err) => err.response);
+}
+
+export function getFilledSlots(dateRange) {
+  return axios
+    .post(baseUrl + '/filled', { dateRange })
+    .catch((err) => err.response);
+}
+
+export function rescheduleUserAppointment(appointmentId, newDateTime) {
+  return axios
+    .post(baseUrl + '/reschedule', { appointmentId, newDateTime })
+    .catch((err) => err.response);
+}
+
+export function cancelUserAppointment(appointmentId) {
+  return axios
+    .post(baseUrl + '/cancel', { appointmentId })
+    .catch((err) => err.response);
+}
