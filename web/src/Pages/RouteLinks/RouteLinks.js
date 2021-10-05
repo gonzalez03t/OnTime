39b64ec78a6e7ddcacb38bbf3ea44dd3ba03f49e
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2021-10-05 13:32:17 -0700

    added nav, cleaned up some logic

--- web/src/Pages/RouteLinks/RouteLinks.js
+++ web/src/Pages/RouteLinks/RouteLinks.js
@@ -1,10 +1,33 @@
-import NavBar from '../../components/NavBar/NavBar';
+import { Link } from 'react-router-dom';
 
 export default function RouteLinks() {
   return (
     <div className="RouteLinks">
-      <NavBar />
-      <p>Links</p>
+      <p>Links:</p>
+
+      <ul>
+        <li>
+          <Link to="/">Landing Page</Link>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/sign_up">Register</Link>
+        </li>
+
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+
+        <li>
+          <Link to="/test">Test NSFW Image Detection</Link>
+        </li>
+      </ul>
     </div>
   );
 }
