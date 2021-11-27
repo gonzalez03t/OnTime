d3b29dd2be87c2ecaf0cd90e560dc5e8d1a79be4
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2021-11-27 13:31:19 -0700

    cancelAppointment.ts now accepts employee as caller

--- backend/src/endpoints/appointment/cancelAppointment.ts
+++ backend/src/endpoints/appointment/cancelAppointment.ts
@@ -1,39 +1,54 @@
 import { Request, Response } from 'express';
 import { em } from '../..';
 import { Appointment, AppointmentStatus } from '../../entities/Appointment';
+import { User } from '../../entities/User';
 import { getSessionUser } from '../../util/session';
 
+type UserOrId = User | string;
+
+async function fetchAppointment(client: UserOrId, id: string, employee?: User) {
+  let where = {
+    id,
+    client,
+    status: AppointmentStatus.PENDING,
+  } as any;
+
+  if (typeof client === 'string') {
+    where.employee = employee;
+  }
+
+  return em.findOne(Appointment, where, ['reminders']);
+}
+
 /**
- * This function will attempt to cancel an appointment for the logged in user
+ * This function will attempt to cancel an appointment for the logged in user, or
+ * the client specified by the clientId body parameter.
  */
 export default async function cancelAppointment(req: Request, res: Response) {
   const user = await getSessionUser(req);
 
-  const { appointmentId } = req.body;
+  const { clientId, appointmentId } = req.body;
 
   if (!appointmentId) {
     res.status(400).send('You must specify an appointment to cancel.');
-  } else if (user) {
-    const appt = await em.findOne(
-      Appointment,
-      {
-        client: user,
-        id: appointmentId,
-        status: AppointmentStatus.PENDING,
-      },
-      ['reminders'] // load the reminders so it can be altered in cancel
-    );
+  } else if (!user) {
+    res.sendStatus(401);
+  } else {
+    // client is either the viewer, or loaded from body param
+    let client = clientId ?? user;
+    // employee can be the viewer, otherwise it will not be used
+    let employee = clientId ? user : undefined;
 
-    if (appt) {
-      appt.cancel();
+    let appointment = await fetchAppointment(client, appointmentId, employee);
+
+    if (appointment) {
+      appointment.cancel();
       await em
-        .persistAndFlush(appt)
+        .persistAndFlush(appointment)
         .then(() => res.sendStatus(200))
         .catch((err) => res.status(500).send(err));
     } else {
       res.status(500).send('No appointment could be found.');
     }
-  } else {
-    res.sendStatus(401);
   }
 }
